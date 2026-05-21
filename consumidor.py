2d32c753332811d2ced1771e832e8bcb0dfc9c61
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-05-21 18:47:46 -0300

    chore: deixando ctrl+c bonito visualmente

--- consumidor.py
+++ consumidor.py
@@ -3,18 +3,29 @@ import sys
 import os
 import time
 
-# conexão com o broker RabbitMQ local
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+def main():
+    # conexão com o broker RabbitMQ local
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
 
-# fila 'tarefas' como duravel
-channel.queue_declare(queue='tarefas', durable=True)
+    # fila 'tarefas' como duravel
+    channel.queue_declare(queue='tarefas', durable=True)
 
-def callback(ch, method, properties, body):
-    print(f"Recebido: {body.decode()}")
-    time.sleep(0.5)
+    def callback(ch, method, properties, body):
+        print(f"Recebido: {body.decode()}")
+        time.sleep(0.5)
 
-# configura o consumidor para a fila 'tarefas'
-channel.basic_consume(queue='tarefas', on_message_callback=callback, auto_ack=True)
-print('Aguardando mensagens. Pressione CTRL+C Para sair' )
-channel.start_consuming()
+    # configura o consumidor para a fila 'tarefas'
+    channel.basic_consume(queue='tarefas', on_message_callback=callback, auto_ack=True)
+    print('Aguardando mensagens. Pressione CTRL+C Para sair' )
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrompido')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
