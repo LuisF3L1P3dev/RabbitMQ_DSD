1858e1e3e18a18328a692264d3ea5849f382491d
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-05-21 17:14:04 -0300

    feat: declarando como uma queue duravel e tornando a mensagem persistente

--- produtor.py
+++ produtor.py
@@ -7,18 +7,21 @@ connection = pika.BlockingConnection(
 
 channel = connection.channel()
 
-# cria fila
-channel.queue_declare(queue="tarefas")
+# cria fila e declarando como uma queue duravel
+channel.queue_declare(queue="tarefas", durable=True)
 
 # envia mensagens
 for i in range(5):
     mensagem = f"Tarefa {i}"
     
+
     channel.basic_publish(
         exchange="",
         routing_key="tarefas",
-        body=mensagem
-    )
+        body=mensagem,
+        properties=pika.BasicProperties(      
+            delivery_mode=pika.DeliveryMode.Persistent,  # Torna a mensagem persistente
+        ))
 
     print(f"Enviado: {mensagem}")
 
